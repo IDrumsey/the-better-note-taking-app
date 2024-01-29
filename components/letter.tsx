08e9288200de260210a6e48e44b6e7a72c4045b6
IDrumsey <claydevelop@gmail.com>
2024-01-28 19:23:34 -0500

    letter component

--- components/letter.tsx
+++ components/letter.tsx
@@ -1,11 +1,30 @@
 import { Box } from "@mui/material"
+import { useEffect, useState } from "react"
 
 type Props = {
   text: string
+  index: number
+  onHover: (index: number) => void
 }
 
-const Letter = ({ text }: Props) => {
-  return <Box component="span">{text}</Box>
+const Letter = ({ text, index, onHover }: Props) => {
+  const [hovering, hoveringSetter] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (hovering) {
+      onLetterHover()
+    }
+  }, [hovering])
+
+  const onLetterHover = () => {
+    onHover(index)
+  }
+
+  return (
+    <Box component="span" onMouseEnter={() => hoveringSetter(true)} onMouseLeave={() => hoveringSetter(false)}>
+      {text}
+    </Box>
+  )
 }
 
 export default Letter
