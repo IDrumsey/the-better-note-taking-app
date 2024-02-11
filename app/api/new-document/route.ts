1964b79874d865aa220ecf51a04ef1168c3887b2
IDrumsey <claydevelop@gmail.com>
2024-02-10 21:01:55 -0500

    Add route to handle adding new document

---
+++ app/api/new-document/route.ts
@@ -0,0 +1,65 @@
+import { newDocumentAPISchema } from "@/app/schemas/document"
+import { tryStuff } from "@/app/utility/gen"
+import { createClient } from "@/utils/supabase/server"
+import { cookies } from "next/headers"
+
+export async function POST(request: Request) {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
+
+  // get the request body
+  const requestBody = await request.json()
+
+  // TODO: auth
+
+  // validate the incoming data
+  const [validData, validationError] = tryStuff(newDocumentAPISchema.parse, requestBody)
+
+  if (validationError) {
+    return Response.json({
+      errors: [{ message: "Invalid Data" }],
+    })
+  }
+
+  const userResponse = await supabase.auth.getUser()
+
+  if (userResponse.error) {
+    return Response.json(
+      {
+        errors: [{ message: "Could not determine who is trying to add a document. Are you authenticated?" }],
+      },
+      { status: 401 }
+    )
+  }
+
+  const result = await supabase
+    .from("pages")
+    .insert({
+      title: validData.title,
+      text: validData.text,
+      user_id: userResponse.data.user.id,
+    })
+    .select()
+
+  switch (result.status) {
+    case 201:
+      return Response.json(
+        {
+          data: {
+            documentId: result.data?.at(0)?.id ?? -1,
+          },
+        },
+        {
+          status: 201,
+        }
+      )
+
+    default:
+      return Response.json(
+        {
+          errors: [{ message: "Failed to create new document record" }],
+        },
+        { status: 500 }
+      )
+  }
+}
