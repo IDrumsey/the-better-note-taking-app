23157aedd9b3811a1e29dda80e5c29a12e057612
IDrumsey <claydevelop@gmail.com>
2024-02-10 21:02:12 -0500

    add types for API route input data.

--- app/schemas/document.ts
+++ app/schemas/document.ts
@@ -4,3 +4,8 @@ export const newDocumentSchema = z.object({
   title: z.string().min(1, "Required"),
   text: z.string().min(1, "Required"),
 })
+
+export const newDocumentAPISchema = newDocumentSchema.extend({
+  title: newDocumentSchema.shape.title.pipe(z.coerce.string()),
+  text: newDocumentSchema.shape.text.pipe(z.coerce.string()),
+})
