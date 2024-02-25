12339782d3a72845bcd6fd57908e2694888f99a6
IDrumsey <claydevelop@gmail.com>
2024-02-24 22:06:58 -0500

    Added utility function getMinimumTextFromSelectedText.

--- app/utility/gen.ts
+++ app/utility/gen.ts
@@ -19,3 +19,28 @@ export function tryStuff<T extends any[], K>(
 export function colorToAlphaHex(color: string): string {
   return `${color}28`
 }
+
+/**
+ * Gets the minimum amount of text for the current selection of text
+ *
+ *
+ * @example
+ *
+ * This is some text and some more text.
+ *
+ * Say that both "some" words were selected; This function would return "some text and some"
+ */
+export const getMinimumTextFromSelectedText = (fullText: string, currentlySelectedWordIndeces: Array<number>) => {
+  // find the start and end indeces
+  // sort the selected word indeces and just take the first and last
+  // create a copy of the currently selected words
+  const selectedWordIndecesAsc = [...currentlySelectedWordIndeces]
+
+  // sort the words by the indeces
+  selectedWordIndecesAsc.sort((a, b) => a - b)
+
+  const minStartIndex = selectedWordIndecesAsc[0]
+  const minEndIndex = selectedWordIndecesAsc[selectedWordIndecesAsc.length - 1] + 1
+
+  return fullText.split(" ").slice(minStartIndex, minEndIndex).join(" ")
+}
