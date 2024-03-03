d63d1de02aa1a6d3e0172d88edaf4cb73f753e6c
IDrumsey <claydevelop@gmail.com>
2024-03-03 16:02:47 -0500

    Remove unnecessary code from app page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
-import Nav from "@/components/Nav"
-import TextContext from "@/components/TextContext"
 import { Typography } from "@mui/material"
-import { promises as fs } from "fs"
 
 export default async function Index() {
-  const file = await fs.readFile(process.cwd() + "/test.txt", "utf-8")
-
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <Typography variant="h1">The Better Note Taking App</Typography>
